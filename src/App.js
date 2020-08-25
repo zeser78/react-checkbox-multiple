5f506a38f049af34e60ac083da9d31c6ab6c8b61
Sergio <solivares@xpaso.com>
2020-08-25 15:49:05 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import CheckBox from "./CheckBox";
+import CheckApp from "./CheckApp";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     fruites: [
+  //       { id: 1, value: "banana", isChecked: false },
+  //       { id: 2, value: "apple", isChecked: false },
+  //       { id: 3, value: "mango", isChecked: false },
+  //       { id: 4, value: "grap", isChecked: false },
+  //     ],
+  //   };
+  // }
+
+  // handleAllChecked = (event) => {
+  //   let fruites = this.state.fruites;
+  //   fruites.forEach((fruite) => (fruite.isChecked = event.target.checked));
+  //   this.setState({ fruites: fruites });
+  //   console.log(fruites);
+  // };
+
+  // handleCheckChieldElement = (event) => {
+  //   let fruites = this.state.fruites;
+  //   fruites.forEach((fruite) => {
+  //     if (fruite.value === event.target.value)
+  //       fruite.isChecked = event.target.checked;
+  //   });
+  //   this.setState({ fruites: fruites });
+  //   console.log(fruites);
+  // };
+
+  render() {
+    return (
+      <div className="App">
+        {/* <h1> Check and Uncheck All Example </h1>
+        <input
+          type="checkbox"
+          onChange={this.handleAllChecked}
+          value="checkedall"
+        />{" "}
+        Check / Uncheck All
+        <ul>
+          {this.state.fruites.map((fruite, index) => {
+            return (
+              <CheckBox
+                key={index}
+                handleCheckChieldElement={this.handleCheckChieldElement}
+                {...fruite}
+              />
+            );
+          })}
+        </ul> */}
+        <CheckApp />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/CheckApp.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import CheckBox from "./CheckBox";
+
+const App = () => {
+  const [isChecked, setChecked] = useState(false);
+  const [fruite, setFruites] = useState([
+    { id: 1, value: "banana", isChecked: false, price: 12 },
+    { id: 2, value: "apple", isChecked: false, price: 32 },
+    { id: 3, value: "mango", isChecked: false, price: 42 },
+    // { id: 4, value: "grapefruit" },
+    // { id: 5, value: "grape" },
+  ]);
+
+  const [amount, setAmount] = useState([0]);
+
+  const reducer = (accumulator, currentValue) => accumulator + currentValue;
+
+  const handleAllChecked = (event) => {
+    // useEffect(() => {
+    let fruites = fruite;
+    let count = [0];
+    fruites.forEach((fruite) => {
+      fruite.isChecked = event.target.checked;
+      if (fruite.isChecked == true) {
+        count.push(fruite.price);
+      }
+      console.log(fruite.price);
+    });
+    setFruites(fruites);
+    setChecked(!isChecked);
+    setAmount(count);
+    console.log(count);
+    // console.log(fruites)
+    // }, []);
+  };
+
+  const handleCheckChieldElement = (event) => {
+    console.log(event.target.checked);
+    // setChecked(!isChecked);
+    let fruites = fruite;
+    fruites.forEach((fruite) => {
+      if (fruite.value === event.target.value)
+        fruite.isChecked = event.target.checked;
+      // setFruites(fruites);
+      setChecked(!isChecked);
+      console.log(fruite.value, event.target.value, fruites);
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1> Check and Uncheck All Example </h1>
+      <input type="checkbox" onChange={handleAllChecked} value="checkedall" />
+      Check / Uncheck All
+      <ul>
+        {fruite.map((fruite, index) => {
+          return (
+            <>
+              <li>
+                <input
+                  key={index}
+                  onChange={handleCheckChieldElement}
+                  type="checkbox"
+                  checked={fruite.isChecked}
+                  value={fruite.value}
+                />
+                {fruite.value}
+              </li>
+            </>
+            // (<p>total : {fruite.price}</p>)
+          );
+        })}
+      </ul>
+      <p>total : {amount.reduce(reducer)}</p>
+    </div>
+  );
+};
+
+export default App;
